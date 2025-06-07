3a1af49b959e63d3460b30fee2e5e42c58ecf9d9
Shivam Mehta <sm.cse17@gmail.com>
2025-06-08 03:46:06 +0530

    feat(plugins): Add "nvzone/showkeys"

---
+++ lua/plugins/showkeys.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvzone/showkeys",
+    cmd = "ShowkeysToggle",
+    opts = {
+      maxkeys = 3,
+      show_count = true,
+      excluded_modes = { "i" },
+      winopts = {
+        border = "rounded",
+      },
+    },
+  }
+}
