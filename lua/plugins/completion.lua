c518f84426e1a33f1b5477f66a8242ba7cae4ea6
Shivam Mehta <sm.cse17@gmail.com>
2025-06-15 00:56:20 +0530

    chore(completion): make nvim-cmp snippets not cry on tabs
    Before:
    If you're in middle of a snippet, and you've indeed found something you
    want to use for that "code location" in the snippet, you'd normally
    switch to next "code location" (if present) within the snippet, but it'd
    not let you because Tab/S-Tab are tied to moving to next/previous entry
    in the completion list, hence they break the whole point of using a
    snippet!
    
    After:
    We just disable the bindings for Tab/S-Tab, and avoid this problem
    altogether. If we do need to go to next/previous entry in the completion
    list, we'll basically use the Ctrl-N/Ctrl-P (which are the defaults)

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -142,24 +142,6 @@ return {
               end
             end,
           }),
-
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
         }),
 
         sorting = {
